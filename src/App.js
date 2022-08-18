5e2bc3be903336ee8c9ab79697842d404b1df27b
usm2198 <101632664+usm2198@users.noreply.github.com>
2022-08-18 11:38:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Umbrella from './components/umbrella';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <Umbrella />
+      </div>
+    </div>
+  );
+}
+
+export default App;
